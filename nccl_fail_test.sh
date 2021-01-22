1f225dcfe7007d390791fc7ff4de52993f7abe51
Steve Farrell <sparticlesteve@gmail.com>
2021-01-22 10:07:55 -0800

    update nccl error script

--- nccl_fail_test.sh
+++ nccl_fail_test.sh
@@ -7,14 +7,19 @@
 #SBATCH --time 30
 #SBATCH -o nccl-test-%j.out
 
+# Software
 module load cgpu pytorch/1.7.1-gpu
 module list
 
+# NCCL debug output
 export NCCL_DEBUG=INFO
 #export NCCL_DEBUG_SUBSYS=ALL
 
+# Dump some information
 echo "DDP NCCL training test"
+srun -N 1 -n 1 python pytorch_info.py
 
+# Run a distributed training test
 srun -u -l python test_ddp.py --gpu --backend nccl --init-method slurm
 
 echo "All done!"
