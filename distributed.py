f16524b563be552c8ce38e2f44aaab4c5e671a90
Steve Farrell <sparticlesteve@gmail.com>
2021-01-22 10:05:20 -0800

    adding some printouts for the distributed init

--- distributed.py
+++ distributed.py
@@ -29,12 +29,14 @@ def init_workers(backend, init_method, port='29500'):
         world_size = int(os.environ['SLURM_NTASKS'])
         os.environ['MASTER_ADDR'] = os.environ['SLURM_LAUNCH_NODE_IPADDR']
         os.environ['MASTER_PORT'] = port
+        print(f'Distributed init with master addr {os.environ["MASTER_ADDR"]} port {port}')
         init_args.update(rank=rank, world_size=world_size)
 
     elif init_method == 'file':
         rank = int(os.environ['SLURM_PROCID'])
         world_size = int(os.environ['SLURM_NTASKS'])
         sync_file = _get_sync_file()
+        print(f'Distributed init with sync file {sync_file}')
         init_args.update(rank=rank, world_size=world_size, init_method=sync_file)
 
     # Initialize the distributed backend
