f9956867aac8902c906226317dd3126a3c936109
Steve Farrell <sparticlesteve@gmail.com>
2021-02-03 22:50:29 -0800

    update nccl fail test script

--- nccl_fail_test.sh
+++ nccl_fail_test.sh
@@ -4,23 +4,27 @@
 #SBATCH --ntasks-per-node 8
 #SBATCH --gpus-per-task 1
 #SBATCH --cpus-per-task 10
-#SBATCH --time 30
+#SBATCH --time 5
 #SBATCH -o nccl-test-%j.out
 
 # Software
 module load cgpu pytorch/1.7.1-gpu
 module list
 
+set -x
+
 # NCCL debug output
-#export NCCL_DEBUG=INFO
-#export NCCL_DEBUG_SUBSYS=ALL
+export NCCL_DEBUG=INFO
+#export NCCL_IB_DISABLE=0
+#export NCCL_SOCKET_IFNAME=eth,ib
 
 # Dump some information
-echo "DDP NCCL training test"
 srun -N 1 -n 1 python pytorch_info.py
 
 # Run a distributed training test
 srun -u -l python test_ddp.py --gpu --backend nccl --init-method slurm \
     --ranks-per-node $SLURM_NTASKS_PER_NODE
+
+set +x
 
 echo "All done!"
