4f545c2c970bfc4ea561272625ef3401678e0322
Steve Farrell <sparticlesteve@gmail.com>
2024-03-27 14:18:19 -0700

    update mpi4py test

--- scripts/run_mpi4py_test.sh
+++ scripts/run_mpi4py_test.sh
@@ -2,7 +2,11 @@
 #SBATCH -J mpi4py-test
 #SBATCH -C gpu
 #SBATCH -t 30
-#SBATCH -N 1 -n 1 -G 1 -c 10
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=32
+#SBATCH --module=gpu
 #SBATCH -o logs/%x-%j.out
 
 echo "---------------------------------------------------------------"
@@ -11,8 +15,14 @@ echo "MPI4Py test"
 echo "Cluster: $SLURM_CLUSTER_NAME"
 echo "Nodes: $SLURM_JOB_NODELIST"
 echo "Tasks: $SLURM_NTASKS"
+echo "Image: $SLURM_SPANK_SHIFTER_IMAGEREQUEST"
 echo "Extra args: $@"
 module list
 
+[ -z $SLURM_SPANK_SHIFTER_IMAGE ] || SHIFTER=shifter
+export MASTER_ADDR=$(hostname)
+export MASTER_PORT=29507
+export MPICH_GPU_SUPPORT_ENABLED=0
+
 set -x
-srun -l -u python -m mpi4py.bench helloworld $@
+srun -l -u --mpi=pmi2 ${SHIFTER} python -m mpi4py.bench helloworld $@
